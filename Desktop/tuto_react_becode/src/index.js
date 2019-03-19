b7d806e5dcdcf726b0ffefd75de7271a53952b13
matthieu <matthieu.jasselette@gmail.com>
2019-03-19 11:37:14 +0100

    initial commit

---
+++ Desktop/tuto_react_becode/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Welcome from './Welcome';
+
+ReactDOM.render(<Welcome />, document.getElementById('root'));
